562139b90f230f4bb9b684eca0d9c0f335780e52
Михаил Драгункин <contact@md.land>
2025-12-30 12:17:37 +0300

    refactoring: move `CaseInsensitiveDict` tests to other unit tests file

--- changelog.md
+++ changelog.md
@@ -11,6 +11,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - [x] fix: module marker exception usage
 - [x] enhancement: switch `merge` method from recursion to cycle
 - [x] enhancement: switch `flat` method from recursion to cycle
+- [x] refactoring: move `CaseInsensitiveDict` tests to other unit tests file
 
 ## [1.0.0] - 2023-01-27
 
--- tests/unit/md/python/dict.py
+++ tests/unit/md/python/dict.py
@@ -26,6 +26,34 @@ def dataset(data_set: typing.Dict[str, typing.Dict[str, typing.Any]]) -> pytest.
 
 
 # Tests:
+class TestCaseInsensitiveDict:
+    @pytest.mark.parametrize('key', ['key', 'keY', 'kEy', 'kEY', 'Key', 'KeY', 'KEy', 'KEY'])
+    def test_case_insensitive_dict_access_after_init(self, key: str) -> None:
+        # act
+        case_insensitive_dict = md.python.dict.CaseInsensitiveDict()
+        case_insensitive_dict['key'] = 42
+
+        # assert
+        assert case_insensitive_dict[key] == 42
+
+    @pytest.mark.parametrize('key', ['key', 'keY', 'kEy', 'kEY', 'Key', 'KeY', 'KEy', 'KEY'])
+    def test_case_insensitive_dict_access_after_init_from_dict(self, key: str) -> None:
+        # act
+        case_insensitive_dict = md.python.dict.CaseInsensitiveDict(dict_={'key': 42})
+
+        # assert
+        assert case_insensitive_dict[key] == 42
+
+    @pytest.mark.parametrize('key', ['key', 'keY', 'kEy', 'kEY', 'Key', 'KeY', 'KEy', 'KEY'])
+    def test_case_insensitive_dict_delete_key(self, key: str) -> None:
+        # act
+        case_insensitive_dict = md.python.dict.CaseInsensitiveDict(dict_={'key': 42})
+        del case_insensitive_dict['key']
+
+        # assert
+        assert key not in case_insensitive_dict
+
+
 class TestDefaultMergeDictionary:
     def test_merge(self) -> None:
         # arrange
